eafdc089c4e96756be918bfd243698b0d83ea3f0
Marfatia <Ankur.X.Marfatia@delta.com>
2016-05-05 13:54:59 -0400

    First commit

---
+++ src/com/sample/Application.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Application {
+
+	public Application() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
